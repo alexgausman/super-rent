52e6b018655a2d246e38f87c5d215cb3455e717c
agaus <gausman.a@gmail.com>
2019-11-15 16:35:31 -0800

    Move countReqVehicles into utils

--- backend/routes/customerActions.js
+++ backend/routes/customerActions.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { database } = require('../config');
 const formatQuery = require('../utils/formatQuery');
+const countReqVehicles = require('../utils/countReqVehicles');
 
 // @route   POST find-available-vehicles
 // @desc    List available vehicles
@@ -25,33 +26,5 @@ router.post('/find-available-vehicles', (req, res) => {
       error_message: error.message,
     }));
 });
-
-// Desc: Returns number of required vehicles to fulfill reservations
-//       where all reservations are for a single vehicle type.
-const countReqVehicles = reserves => {
-  const multipleVReserves = [];
-  while (reserves.length > 0) {
-    const singleVReserves = [reserves.shift()];
-    while (true) {
-      let prevRes = singleVReserves[singleVReserves.length -1];
-      let nextResIndex;
-      for(let i = 0; i < reserves.length; i++) {
-        const reserve = reserves[i];
-        if (new Date(reserve.fromDateTime) >= new Date(prevRes.toDateTime)) {
-          nextResIndex = i;
-          break;
-        }
-      }
-      if (nextResIndex) {
-        const nextRes = reserves.splice(nextResIndex, 1)
-        singleVReserves.push(nextRes[0]);
-      } else {
-        break;
-      }
-    }
-    multipleVReserves.push(singleVReserves);
-  }
-  return multipleVReserves.length;
-}
 
 module.exports = router;
---
+++ backend/utils/countReqVehicles.js
@@ -0,0 +1,30 @@
+// Desc: Computes number of required vehicles to fulfill reservations
+//       where all reservations are for a single vehicle type.
+// Param: Array of objects { fromDateTime: string, toDateTime: string, ... }
+const countReqVehicles = reserves => {
+  const multipleVReserves = [];
+  while (reserves.length > 0) {
+    const singleVReserves = [reserves.shift()];
+    while (true) {
+      let prevRes = singleVReserves[singleVReserves.length -1];
+      let nextResIndex;
+      for(let i = 0; i < reserves.length; i++) {
+        const reserve = reserves[i];
+        if (new Date(reserve.fromDateTime) >= new Date(prevRes.toDateTime)) {
+          nextResIndex = i;
+          break;
+        }
+      }
+      if (nextResIndex) {
+        const nextRes = reserves.splice(nextResIndex, 1)
+        singleVReserves.push(nextRes[0]);
+      } else {
+        break;
+      }
+    }
+    multipleVReserves.push(singleVReserves);
+  }
+  return multipleVReserves.length;
+}
+
+module.exports = countReqVehicles;
